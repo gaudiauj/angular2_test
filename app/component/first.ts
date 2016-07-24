4dd31de92dfe3f111d505914c2d4a8fcb4304a23
jean <gaudiauj@gmail.com>
2016-07-24 20:45:01 +0200

    test en fonction de la premiere partie du tuto angular 2: ngmodel et binding

--- app/component/first.ts
+++ app/component/first.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 @Component({
-    template: '<h1>My First Angular 2 App</h1>',
+    template: `<h1>{{title}}</h1> 
+    <div>
+        <span>{{heroes.yoman.name}} : {{heroes.yoman.sayHI()}}</span>
+        <label><input [(ngModel)]="heroes.yoman.name" placeholder="name"> name</label> 
+        
+    </div>
+    <span>{{heroes.yowoman.name}}  : {{heroes.yowoman.sayHI()}}</span>
+`,
     selector: 'my-app'
 })
 export class AppComponent {
 
+    title = 'couco afau'
+    heroes = {
+        yoman: {
+            name : 'yoman fezf ',
+            sayHI : function() {
+                return 'yo aezrferfa '+this.name
+            }
+        },
+        yowoman: {
+            name : 'yowoman',
+            sayHI : function() {
+                return 'yo'+this.name
+            }
+        }
+    }
+
     constructor () {
-        console.log('youhou')
+        console.log('youhazdzaou')
     }
 }
+
