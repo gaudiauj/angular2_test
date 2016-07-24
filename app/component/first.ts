aeeb78f68896bef6bdba369667435348528586a5
jean gaudiau <jean gaudiau>
2016-07-24 18:39:28 +0200

    first commit gulpfile et boostrap d'une app

---
+++ app/component/first.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+    template: '<h1>My First Angular 2 App</h1>',
+    selector: 'my-app'
+})
+export class AppComponent {
+
+    constructor () {
+        console.log('youhou')
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var watchify = require("watchify");
+var tsify = require("tsify");
+var gutil = require("gulp-util");
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+var pump = require('pump');
+var connect = require('gulp-connect');
+var paths = {
+    html: ['app/*.html'],
+    ts: ['app/test.ts']
+};
+
+var myBrowserify = browserify({
+    basedir: '.',
+    debug: true,
+    entries: paths.ts,
+    cache: {},
+    packageCache: {}
+});
+
+var watchedBrowserify = watchify(myBrowserify.plugin(tsify));
+
+gulp.task('connect', function() {
+    connect.server({
+        livereload: true
+    });
+});
+gulp.task("copy-html", function () {
+    return gulp.src(paths.html)
+        .pipe(gulp.dest("build"));
+});
+gulp.task('browserify', function() {
+    return myBrowserify
+        .plugin(tsify)
+        .bundle()
+})
+gulp.task('uglify', function (cb) {
+    pump([
+            gulp.src('build/bundle.js'),
+            uglify(),
+            gulp.dest('build')
+        ],
+        cb
+    );
+});
+
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest("build"))
+        .pipe(connect.reload());
+}
+
+
+gulp.task("build", ["connect","copy-html","browserify"], function() {
+    console.log('building ...');
+});
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
